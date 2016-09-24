980f5723dc658a11f3aeb550613e61ed884a5b53
hesparza <hiram.esparza@gmail.com>
2016-09-24 13:14:10 -0500

    Addint TempLogEntryDao and TempLogController

---
+++ src/main/java/com/cjlyth/sousvide/api/controller/TempLogController.java
@@ -0,0 +1,43 @@
+package com.cjlyth.sousvide.api.controller;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cjlyth.sousvide.api.entity.TempLog;
+import com.cjlyth.sousvide.api.service.TempLogService;
+
+@RestController
+@RequestMapping(value = "tempLog")
+public class TempLogController {
+
+	@Autowired
+	TempLogService tempLogService;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void saveTempLog(@RequestBody TempLog tempLog) {
+		tempLogService.save(tempLog);;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public Collection<TempLog> findAll() {
+		return tempLogService.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
+	public Collection<TempLog> findAllById(@PathVariable("id") Integer id) {
+		return tempLogService.findAllById(id);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/{fromTime}/{toTime}")
+	public Collection<TempLog> findAllByFromTimeAfterAndToTimeBefore(@PathVariable("fromTime") Date fromTime, @PathVariable("toTime") Date toTime) {
+		return tempLogService.findAllByFromTimeAfterAndToTimeBefore(fromTime, toTime);
+	}
+}
